0f916add36376a8475ccb935d4fbdd9be03f685b
Chris Linnell <chrislinnell232@gmail.com>
2024-02-13 14:23:07 +0000

    Update models.py

--- website/accounts/models.py
+++ website/accounts/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .managers import CustomUserManager
 
-
-
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique = True)
